c2ec8fe1d252a8f27c48f16ef45d43467867f9df
chethankailash <chethankailash3@gmail.com>
2018-08-16 21:08:59 +0530

    model class created

---
+++ models/ticket.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const ticketSchema = new Schema({
+	name:{
+		type:String,
+		required:true
+	},
+	department:{
+		type:String,
+		enum:['Technical','Sales','Hr'],
+		required:true
+	},
+	priority:{
+		type:String,
+		enum:['High','Medium','Low'],
+		required:true
+	},
+	message:{
+		type:String,
+		required:true,
+		minlength:5
+	},
+	status:{
+		type:String,
+		default:'open'
+	}
+});
+
+const Ticket = mongoose.model('ticket',ticketSchema);
